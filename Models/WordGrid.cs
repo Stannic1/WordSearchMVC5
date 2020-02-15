53961d83c053843a4159a822369c5dd634d0cc13
Stannic1 <VienLien52@gmail.com>
2020-02-15 16:25:53 -0600

    Test Commit; Added WordGrid Model

---
+++ Models/WordGrid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WordSearchMVC5.Models
+{
+    public class WordGrid
+    {
+        public List<char[]> UserWords { get; set; }
+        public Boolean ValidWord(char[] userEntry)
+        {
+            char[] reversedEntry = userEntry;
+            Array.Reverse(reversedEntry);
+            return UserWords.Contains(userEntry) || UserWords.Contains(reversedEntry);
+        }
+    }
+}
